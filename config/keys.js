7f93c5d1527d792bdba7cc48ec5cb236420c0524
A.Rupesh <a.rupesh66@gmail.com>
2018-09-21 01:08:50 +0530

    key update(do not touch)

--- config/keys.js
+++ config/keys.js
@@ -1,1 +1,8 @@
 
+module.exports = {
+    mongoURI:'mongodb://rupeshjs:rupesh1310@ds263832.mlab.com:63832/forum-dev',
+    googleClientID: '877190760211-m400vf8ddggu92rgpq2f37lk9g2dacoh.apps.googleusercontent.com',
+    googleClientSecret: 'z_Sed5hPJ-Bt_Z4mZL5gHvnh'
+    
+    
+}
