99610079de6651e094016fb42002cc68aa134b98
A.Rupesh <a.rupesh66@gmail.com>
2018-09-21 01:07:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+//Passport COnfig
+require('./config/passport')(passport);
+
+
+
+//Load Routes
+const auth = require('./routes/auth');
 
 const app = express();
 
@@ -7,6 +16,7 @@ app.get('/', (req, res) => {
     res.send('It Works!');
 });
 
+app.use('/auth', auth);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
